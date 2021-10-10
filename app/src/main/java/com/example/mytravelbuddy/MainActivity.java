f210bffaa4a9a0cfc210a9819aef8f0ea6884bfc
AnwarChong <AnwarBinBujang@users.noreply.github.com>
2021-10-11 00:40:50 +0800

    Try Camera Streaming

--- app/src/main/java/com/example/mytravelbuddy/MainActivity.java
+++ app/src/main/java/com/example/mytravelbuddy/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mytravelbuddy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.example.mytravelbuddy.ui.OcrDetectorProcessor;
 import com.example.mytravelbuddy.ui.home.HomeFragment;
 import com.example.mytravelbuddy.ui.map.Map_search;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -19,12 +21,17 @@ import com.huawei.hms.mlplugin.asr.MLAsrCaptureActivity;
 import com.huawei.hms.mlplugin.asr.MLAsrCaptureConstants;
 import com.huawei.hms.mlsdk.asr.MLAsrConstants;
 import com.huawei.hms.mlsdk.asr.MLAsrRecognizer;
+import com.huawei.hms.mlsdk.common.LensEngine;
 import com.huawei.hms.mlsdk.common.MLApplication;
+import com.huawei.hms.mlsdk.text.MLTextAnalyzer;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    FloatingActionButton mic;
+    FloatingActionButton camera;
     TextView textView3;
+    MLTextAnalyzer analyzer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,21 +66,54 @@ public class MainActivity extends AppCompatActivity {
 
 
         //for mic
-        mic=findViewById(R.id.mic);
-        mic.setOnClickListener(new View.OnClickListener() {
+        camera=findViewById(R.id.camera);
+
+        camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onMicOpen();
+                onCameraStream();
+
+
             }
         });
 
 
     }
 
-    public void onMicOpen(){
+    public void onCameraStream(){
+        MLTextAnalyzer analyzer = new MLTextAnalyzer.Factory(MainActivity.this).create();
 
+        analyzer.setTransactor(new OcrDetectorProcessor());
+        LensEngine lensEngine = new LensEngine.Creator(getApplicationContext(),analyzer)
+                .setLensType(LensEngine.BACK_LENS)
+                .applyDisplayDimension(1440, 1080)
+                .applyFps(30.0f)
+                .enableAutomaticFocus(true)
+                .create();
+
+
+
+        SurfaceView mSurfaceView = findViewById(R.id.surface_view);
+        try {
+            lensEngine.run(mSurfaceView.getHolder());
+        } catch (IOException e) {
+            // Exception handling logic.
+        }
+
+        if (analyzer != null) {
+            try {
+                analyzer.stop();
+            } catch (IOException e) {
+                // Exception handling.
+            }
+        }
+        if (lensEngine != null) {
+            lensEngine.release();
+        }
 
     }
+
+
 
     private boolean loadFragment(Fragment fragment) {
         //switching fragment
